1c7837bd58ae3004bcf6556e0f810e18a5d87ac8
BlueAdmin <blueadmin@BLUE2016>
2020-06-24 21:41:07 -0500

    Setup example warning; sql error

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,23 @@ namespace OnlineBuild.Pages
         public void OnGet()
         {
 
+        }
+
+        public object TriggerSecurityWarning(
+            string connection, string name, string password)
+        {
+            SqlConnection someConnection = new SqlConnection(connection);
+            SqlCommand someCommand = new SqlCommand();
+            someCommand.Connection = someConnection;
+
+            someCommand.CommandText = "SELECT SecretInfo FROM Users " +
+                "WHERE Username='" + name +
+                "' AND Password='" + password + "'";
+
+            someConnection.Open();
+            object secretInfo = someCommand.ExecuteScalar();
+            someConnection.Close();
+            return secretInfo;
         }
     }
 }
